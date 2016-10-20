c450c0dc0659f0be7fe6b348f2a9b252d6a9f42b
990907yang <yam09071004@gmail.com>
2016-10-20 21:05:03 +0900

    3페이지 추가

--- page/home/main.php
+++ page/home/main.php
@@ -22,7 +22,36 @@
 			</ul>
 		</div>
 	</div>
-	<div class="section s3">WHATEVER</div>
+	<div class="section s3">
+	<div class="fp-slides">
+			<div class="slide_wrap">
+				<div class="slide fp-slide sl">
+				    <div class="ub_box">
+                        <div class="ub_list"></div>
+                        <div class="ub_list"></div>
+                        <div class="ub_list"></div>
+		            </div>
+				</div>
+				<div class="slide fp-slide sl ub_box2">
+                    <div class="ub_inbox">
+                        <div class="table1 a">1</div>
+                        <div class="table2 a">2</div>
+                        <div class="table3 a">3</div>
+                        <div class="table4 a">4</div>
+                        <div class="table5 a">5</div>
+                        <div class="table6 a">6</div>
+                    </div>
+				</div>
+			</div>
+		</div>
+<!--
+        <h2>유비쿼터스과</h2>
+        
+       <div class="ub_box2">
+           asd
+       </div>
+-->
+    </div>
 	<div class="section s4">WHATEVER</div>
 	<div class="section s5">WHATEVER</div>
 </div>
