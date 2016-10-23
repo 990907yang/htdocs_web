185bfd0dd68ce979dea7ff6ddd3e794efe4b1091
990907yang <yam09071004@gmail.com>
2016-10-23 22:11:21 +0900

    게임과 슬라이드 대충

--- common/js/main.js
+++ common/js/main.js
@@ -22,4 +22,31 @@ $(document).ready(function(){
 	window.onload = function(){
 		$('.slide_wrap h2').css({"opacity":1,"margin-bottom":"0"});
 	}
+    
+    var current = 0; max = $(".game_list li").length-1; width=1200;
+    
+    function next(){
+        if( current != max ){
+            slide( ++current );
+        }else{
+            current=0;
+            slide(0);
+        }
+    }function prev(){
+        if( current != 0 ){
+            slide(--current);
+        }else{
+            current=max;
+            slide(max);
+        }
+    }
+    $(".s2_sl_btnl").click(function(){
+        prev();
+    });
+    $(".s2_sl_btnr").click(function(){
+        next();
+    });
+    function slide(idx){
+        $(".game_list").stop().animate({"left":-(idx*width)+"px"},300);
+    }
 });
--- page/home/main.php
+++ page/home/main.php
@@ -13,15 +13,27 @@
 		</div>
 	</div>
 	<div class="section s2">
-		<h2>게임영상학과</h2>
-		<div class="game_box">
-			<ul class="game_list">
-			    <li class="g_1"><img src="../../common/images/sky.jpg" alt="sky" title="sky"></li>
-			    <li class="g_2"><img src="../../common/images/sky.jpg" alt="sky" title="sky"></li>
-			    <li class="g_3"><img src="../../common/images/sky.jpg" alt="sky" title="sky"></li>
-			</ul>
-		</div>
+        <div class="fp-slides">
+            <div class="slide_wrap">
+                <div class="slide fp-slide sl">
+		          <h2>게임영상학과</h2>
+                        <div class="game_box">
+                            <div class="s2_sl_btnr"></div>
+                            <div class="s2_sl_btnl"></div>
+                            <ul class="game_list">
+                                <li class="g_1">1</li>
+                                <li class="g_2">2</li>
+                                <li class="g_3">3</li>
+                            </ul>
+                        </div>
+                    <div class="sec2_2">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
 	</div>
+    
 	<div class="section s3">
 	<div class="fp-slides">
 			<div class="slide_wrap">
